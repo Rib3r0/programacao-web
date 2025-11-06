f49d8b289c6b66348f7e42042cfeb4252e685e18
Pimenta Eduardo <eduardo-e.pimenta@thalesgroup.com>
2025-11-06 17:34:41 -0300

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+  if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20) {
+    document.getElementById("header").classList.add('hidden-header')
+    document.getElementById("logo").classList.add('hidden-logo')
+    document.getElementById("nav").classList.add('hidden-nav')
+    console.log("1")
+  } else {
+    document.getElementById("header").classList.remove('hidden-header')
+    document.getElementById("logo").classList.remove('hidden-logo')
+    document.getElementById("nav").classList.remove('hidden-nav')
+    console.log("2")
+  }
+}
