82468fa9290b762cadcb614710f6b17f248e3852
Pimenta Eduardo <eduardo-e.pimenta@thalesgroup.com>
2025-11-07 16:42:43 -0300

    falta os destaques e um podcast escutavel

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
-  if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20) {
+  if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
     document.getElementById("header").classList.add('hidden-header')
     document.getElementById("logo").classList.add('hidden-logo')
     document.getElementById("nav").classList.add('hidden-nav')
